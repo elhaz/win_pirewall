92788a3d4d8a47ea06a28c3be134dc404a66e0b9
XHH <sa2622@naver.com>
2025-01-23 20:24:32 +0900

    윈도우 위치 저장 및 복원 기능 추가, pywin32 의존성 추가

--- watcher.py
+++ watcher.py
@@ -5,6 +5,10 @@ import sys
 import time
 import psutil
 import signal
+import json
+import win32gui
+import win32con
+import win32process  # 추가
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
@@ -13,24 +17,79 @@ class RestartHandler(FileSystemEventHandler):
         self.command = command
         self.process = None
         self.last_restart = 0
-        self.cooldown = 1  # 1초 쿨다운
+        self.cooldown = 1
+        self.window_pos_file = 'window_position.json'
+        self.window_title = None  # 윈도우 타이틀 저장 변수
         self.start_process()
+        
+    def find_window_title(self):
+        if self.process:
+            def callback(hwnd, _):
+                if win32gui.IsWindowVisible(hwnd):
+                    title = win32gui.GetWindowText(hwnd)
+                    # 프로세스 ID로 매칭
+                    _, pid = win32process.GetWindowThreadProcessId(hwnd)  # win32process 사용
+                    if pid == self.process.pid:
+                        self.window_title = title
+                        return False
+            win32gui.EnumWindows(callback, None)
+        
+    def save_window_position(self):
+        if self.process and self.window_title:
+            def callback(hwnd, _):
+                if win32gui.IsWindowVisible(hwnd):
+                    title = win32gui.GetWindowText(hwnd)
+                    if self.window_title == title:
+                        rect = win32gui.GetWindowRect(hwnd)
+                        pos = {
+                            'title': self.window_title,
+                            'x': rect[0],
+                            'y': rect[1],
+                            'width': rect[2] - rect[0],
+                            'height': rect[3] - rect[1]
+                        }
+                        with open(self.window_pos_file, 'w') as f:
+                            json.dump(pos, f)
+                        return False
+            win32gui.EnumWindows(callback, None)
+            
+    def restore_window_position(self):
+        try:
+            with open(self.window_pos_file, 'r') as f:
+                pos = json.load(f)
+                self.window_title = pos.get('title')
+                
+            def callback(hwnd, _):
+                if win32gui.IsWindowVisible(hwnd):
+                    title = win32gui.GetWindowText(hwnd)
+                    if self.window_title == title:
+                        win32gui.SetWindowPos(hwnd, win32con.HWND_TOP,
+                                            pos['x'], pos['y'],
+                                            pos['width'], pos['height'],
+                                            win32con.SWP_SHOWWINDOW)
+                        return False
+            time.sleep(0.5)
+            win32gui.EnumWindows(callback, None)
+        except (FileNotFoundError, json.JSONDecodeError):
+            pass
 
     def start_process(self):
-        """Start the process."""
         if self.process is None:
             print("Starting process...")
-            # Windows에서 새 프로세스 그룹 생성
             CREATE_NEW_PROCESS_GROUP = 0x00000200
             self.process = subprocess.Popen(
                 self.command.split(),
                 creationflags=CREATE_NEW_PROCESS_GROUP
             )
             self.last_restart = time.time()
+            time.sleep(1)  # 윈도우가 생성될 때까지 대기
+            self.find_window_title()  # 윈도우 타이틀 찾기
+            self.restore_window_position()
             
     def stop_process(self):
         if self.process:
             try:
+                self.save_window_position()
                 parent = psutil.Process(self.process.pid)
                 for child in parent.children(recursive=True):
                     try:
